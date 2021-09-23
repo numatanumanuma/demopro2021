c4d9a99321325e0cf98f2f0fcfbbf3db50114900
fujita-k <fujita-k@roboken.iit.tskuba.ac.jp>
2021-09-23 13:24:16 +0900

    サーボモータ

---
+++ human_disinfector/include/human_disinfector/FJT.h
@@ -0,0 +1,31 @@
+#ifndef _FJT_H_
+#define _FJT_H_
+#include <ros/ros.h>
+#include <std_msgs/UInt16.h>
+
+
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+
+class servo_pubNode
+{
+    private:
+        ros::Publisher pub_pattern;
+        std_msgs::UInt16 pattern;
+    
+    public:
+        servo_pubNode()
+        {
+            ros::NodeHandle nh;
+            pub_pattern = nh.advertise<std_msgs::UInt16>("pattern",1000);
+        }
+        
+        void on();
+        void off();
+        void on_and_off();
+
+};
+
+#endif // _FJT_H_
